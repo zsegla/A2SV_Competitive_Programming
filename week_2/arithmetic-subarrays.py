022b9120590fc8da6bacb51f66c40a1bbbb4ad55
Fekreselassie <101448903+zsegla@users.noreply.github.com>
2022-09-23 02:37:45 -0700

    Create arithmetic-subarrays.py

---
+++ week_2/arithmetic-subarrays.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/arithmetic-subarrays/
+
+
+
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        N = len (nums)
+        
+        def good (left, right):
+            k = list(sorted(nums[left:right+1]))
+            for x, y in zip(k, k[1:]):
+                if len(k) == 1:
+                    return True
+                
+            delta = k[1] - k[0]
+            for x, y in zip(k, k[1:]):
+                if y - x != delta:
+                    return False
+            return True
+        
+        result = []
+        for left, right in zip (l,r):
+            result += [good(left, right)]
+        
+        return result
