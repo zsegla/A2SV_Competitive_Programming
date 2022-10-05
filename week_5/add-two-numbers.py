3bddc589422497d228d35bf270d3a473fc17fda1
Fekreselassie <101448903+zsegla@users.noreply.github.com>
2022-10-04 17:59:27 -0700

    Update add-two-numbers.py

--- week_5/add-two-numbers.py
+++ week_5/add-two-numbers.py
@@ -2,7 +2,11 @@
 
 
 
-# definition of singly linked list goes here 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
